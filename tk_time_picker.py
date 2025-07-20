486520d440cbd8f90a9e92e1d812dec6f0506654
yannprada <pradayrol.yannick@gmail.com>
2025-07-20 15:38:39 +0200

    Major:
    TimePicker is now a Button, that opens a dialog to edit the time.
    The same methods should work.

--- tk_time_picker.py
+++ tk_time_picker.py
@@ -9,13 +9,56 @@ class TimePicker(tk.Frame):
     
     def __post_init__(self):
         super().__init__(self.master)
+
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
+        self.display_value = tk.StringVar(value='00:00:00')
+
+        edit_button = tk.Button(self, textvariable=self.display_value, 
+                                command=self.on_edit_button)
+        edit_button.pack(side='right')
+    
+    def on_edit_button(self):
+        dialog = TimePickerDialog(self, self.hours_limit, self.hours, self.minutes, 
+                                  self.seconds)
+        self.wait_window(dialog.root)
+        self.set_value(f'{dialog.hours:02d}:{dialog.minutes:02d}:{dialog.seconds:02d}')
+    
+    def __str__(self):
+        return self.display_value.get()
+
+    def get_seconds(self):
+        raise self.hours * 3600 + self.minutes * 60 + self.seconds
+    
+    def set_value(self, value):
+        self.display_value.set(value)
         
-        inner = tk.Frame(self)
+        value = value.split(':')
+        self.hours = int(value[0])
+        self.minutes = int(value[1])
+        self.seconds = int(value[2])
+
+
+@dataclass
+class TimePickerDialog:
+    master: tk.Widget
+    hours_limit: float = 23
+    hours: int = 0
+    minutes: int = 0
+    seconds: int = 0
+
+    def __post_init__(self):
+        self.root = tk.Toplevel(self.master)
+        self.root.title('Pick a time')
+
+        inner = tk.Frame(self.root)
         inner.pack(padx=5, pady=5)
-        
-        self._hours = UnitPicker(inner, maxi=self.hours_limit, increment2=3)
-        self._minutes = UnitPicker(inner)
-        self._seconds = UnitPicker(inner)
+
+        self._hours = UnitPicker(inner, value=self.hours, maxi=self.hours_limit, 
+                                 increment2=3)
+        self._minutes = UnitPicker(inner, value=self.minutes)
+        self._seconds = UnitPicker(inner, value=self.seconds)
         
         self._hours.pack(side='left')
         tk.Label(inner, text=':').pack(side='left')
@@ -27,18 +70,21 @@ class TimePicker(tk.Frame):
         self._minutes.bind('<<OnWrapUp>>', lambda event: self._hours.add(1))
         self._seconds.bind('<<OnWrapDown>>', lambda event: self._minutes.add(-1))
         self._seconds.bind('<<OnWrapUp>>', lambda event: self._minutes.add(1))
-    
-    def __str__(self):
-        return f'{self._hours}:{self._minutes}:{self._seconds}'
-    
-    def get_seconds(self):
-        return self._hours.value * 3600 + self._minutes.value * 60 + self._seconds.value
-    
-    def set_value(self, value):
-        value = value.split(':')
-        self._hours.set_value(value[0])
-        self._minutes.set_value(value[1])
-        self._seconds.set_value(value[2])
+
+        bottom = tk.Frame(self.root)
+        bottom.pack()
+
+        tk.Button(bottom, text='Cancel', command=self.root.destroy).pack(side='left')
+        tk.Button(bottom, text='OK', command=self.ok).pack(side='left')
+
+        self.root.grab_set()
+
+    def ok(self):
+        self.hours = self._hours.value
+        self.minutes = self._minutes.value
+        self.seconds = self._seconds.value
+        self.root.grab_release()
+        self.root.destroy()
 
 
 @dataclass
@@ -48,7 +94,6 @@ class UnitPicker(tk.Frame):
     increment: int = 1
     increment2: int = 5
     value: int = 0
-    _job_hide_buttons: bool = None
     
     def __post_init__(self):
         super().__init__(self.master)
@@ -68,29 +113,12 @@ class UnitPicker(tk.Frame):
         self._add_button('🠝', self.increment, top, 1)
         self._add_button('🠟🠟', -self.increment2, bottom)
         self._add_button('🠟', -self.increment, bottom, 1)
-        
-        for widget in [self, top, bottom, label]:
-            widget.bind('<Enter>', self.show_buttons)
-            widget.bind('<FocusIn>', self.show_buttons)
-            widget.bind('<Button-1>', self.show_buttons)
-    
-    def show_buttons(self, event):
-        for button in self.buttons:
-            button.grid()
-        
-        if self._job_hide_buttons is not None:
-            self.after_cancel(self._job_hide_buttons)
-        
-        self._job_hide_buttons = self.after(500, self.hide_buttons)
-    
-    def hide_buttons(self):
-        for button in self.buttons:
-            button.grid_remove()
+
+        self._update()
     
     def _add_button(self, symbol, increment, parent, column=0):
         button = tk.Button(parent, text=symbol, command=lambda: self.add(increment))
         button.grid(row=0, column=column)
-        button.after(100, button.grid_remove)
         self.buttons.append(button)
     
     def add(self, amount):
@@ -109,13 +137,6 @@ class UnitPicker(tk.Frame):
     
     def _update(self):
         self.tk_value.set(f'{self.value:02d}')
-    
-    def __str__(self):
-        return self.tk_value.get()
-    
-    def set_value(self, value):
-        self.value = int(value)
-        self._update()
 
 
 if __name__ == '__main__':
